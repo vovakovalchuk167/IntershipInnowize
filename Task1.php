a304ba613381b542453b210f580b303cf7a8af35
Oleg339 <44521022+Oleg339@users.noreply.github.com>
2022-09-09 12:39:41 +0300

    Update Task1.php

--- Task1.php
+++ Task1.php
@@ -6,6 +6,9 @@ class Task1
 {
     public function main(int $number): string
     {
+        if (!is_numeric($number)) {
+            throw new \InvalidArgumentException();
+        }
         $returnValue = ($number <= 10) ? 'Equal or less than 10' : '';
         $returnValue = ($number > 10) ? 'More than 10' : $returnValue;
         $returnValue = ($number > 20) ? 'More than 20' : $returnValue;
