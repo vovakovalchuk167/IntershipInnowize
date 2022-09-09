01ab2eafcc05750360a7aafb280d5ce1b250106a
Oleg339 <44521022+Oleg339@users.noreply.github.com>
2022-09-09 09:31:23 +0300

    Update Task1.php

--- Task1.php
+++ Task1.php
@@ -2,15 +2,13 @@
 
 namespace src;
 
-class Task1{
-
-    public function main(int $number) : string
+class Task1
+{
+    public function main(int $number): string
     {
-        $returnValue = ($number <= 10) ? "Equal or less than 10" : "" ;
-        $returnValue = ($number > 10) ? "More than 10" : $returnValue ;
-        $returnValue = ($number > 20) ? "More than 20" : $returnValue ;
-        $returnValue = ($number > 30) ? "More than 30" : $returnValue;
-
-        return $returnValue;
+        $returnValue = ($number <= 10) ? 'Equal or less than 10' : '';
+        $returnValue = ($number > 10) ? 'More than 10' : $returnValue;
+        $returnValue = ($number > 20) ? 'More than 20' : $returnValue;
+        return ($number > 30) ? 'More than 30' : $returnValue;
     }
 }
