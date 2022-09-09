93231c3f53680cd72954e38bde3ef3b8dd8e0b27
Oleg339 <44521022+Oleg339@users.noreply.github.com>
2022-09-09 12:25:35 +0300

    Update Task2.php

--- Task2.php
+++ Task2.php
@@ -4,23 +4,25 @@ namespace src;
 
 class Task2
 {
-    public function main(string $birthday): int
+    public function main(string $date): int
     {
-        $subBirth = mb_substr($birthday, 0, 6);
-        if (strtotime($birthday) === false) {
+        $subBirth = mb_substr($date, 0, 6);
+        if (strtotime($date) === false) {
             throw new \InvalidArgumentException('Wrong data');
         }
-        $birthday = date('d-m-Y', strtotime($subBirth . (intval(date('Y')))));
-        $birthdate = date_create($birthday);
+        $date = date('d-m-Y', strtotime($subBirth . (intval(date('Y')))));
+        $birthdate = date_create($date);
         $newBirth = date_create();
         $result = intval(date_diff($birthdate, $newBirth)->format('%R%a'));
         if ($result > 0) {
-            $birthday = date('d-m-Y', strtotime($subBirth . (intval(date('Y'))) + 1));
-            $birthdate = date_create($birthday);
+            $date = date('d-m-Y', strtotime($subBirth . (intval(date('Y'))) + 1));
+            $birthdate = date_create($date);
             $newBirth = date_create();
             $result = intval(date_diff($birthdate, $newBirth)->format('%R%a'));
         }
         return abs($result);
     }
 }
+
+
 
