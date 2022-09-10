cf6dfbb40a303a0958feafc15a455293262e379b
Oleg339 <44521022+Oleg339@users.noreply.github.com>
2022-09-10 19:21:06 +0300

    Add files via upload

---
+++ Task6.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace src;
+
+class Task6
+{
+    public function main(int $year, int $lastYear, int $month, int $lastMonth, string $day = 'Monday'): int
+    {
+        if ($year > $lastYear || ($year == $lastYear && $month >= $lastMonth)) {
+            throw new \InvalidArgumentException();
+        }
+        $result = 0;
+        for ($thisYear = $year; $thisYear <= $lastYear; $thisYear++) {
+            for ($thisMonth = $month; $thisMonth <= 12 && ($thisYear != $lastYear || $thisMonth != $lastMonth); $thisMonth++) {
+                $firstDayOfMonth = date('l', strtotime('01-'.$thisMonth.'-'.$thisYear));
+                if ($firstDayOfMonth == $day) {
+                    $result++;
+                }
+            }
+        }
+        return $result;
+    }
+}
+
