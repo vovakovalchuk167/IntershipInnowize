4e1e9114b61908e0b82ae2c9d9172c7b6a41fd47
Oleg339 <44521022+Oleg339@users.noreply.github.com>
2022-09-12 16:37:33 +0300

    Add files via upload

---
+++ Task2.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace src;
+
+class Task2
+{
+    public function main(string $date): int
+    {
+        $subBirth = mb_substr($date, 0, 6);
+        if (strtotime($date) === false || !strtotime($subBirth . (intval(date('Y'))))) {
+            throw new \InvalidArgumentException();
+        }
+        $date = date('d-m-Y', strtotime($subBirth . (intval(date('Y')))));
+        $birthdate = date_create($date);
+        $newBirth = date_create();
+        $result = intval(date_diff($birthdate, $newBirth)->format('%R%a'));
+        if ($result > 0) {
+            $date = date('d-m-Y', strtotime($subBirth . (intval(date('Y'))) + 1));
+            $birthdate = date_create($date);
+            $newBirth = date_create();
+            $result = intval(date_diff($birthdate, $newBirth)->format('%R%a'));
+        }
+
+        return abs($result) + 1;
+    }
+}
