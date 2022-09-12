8290f8683085aef9e444f095e4c294b549be1068
Oleg339 <44521022+Oleg339@users.noreply.github.com>
2022-09-12 19:12:04 +0300

    Add files via upload

---
+++ Task8.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace src;
+
+class Task8
+{
+    public function main(string $json): string
+    {
+        $result = '';
+        $var = json_decode($json, true, 512, JSON_OBJECT_AS_ARRAY);
+        if (!$var || !is_array($var)) {
+            throw new \InvalidArgumentException();
+        }
+        foreach ($var as $key => $value) {
+            if (is_array($value)) {
+                $result .= $this->main(json_encode($value));
+            } else {
+                if (is_int($value)) {
+                    throw new \InvalidArgumentException();
+                }
+                $result .= $key . ': ' . $value . "\r\n";
+            }
+        }
+
+        return substr($result, 0, -1);
+    }
+}
