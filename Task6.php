f7e9c601703ed4bdfd4f13a93cfd67f60a97aa31
Oleg339 <44521022+Oleg339@users.noreply.github.com>
2022-09-12 18:52:37 +0300

    Update Task6.php

--- Task6.php
+++ Task6.php
@@ -7,7 +7,7 @@ class Task6
     public function main(int $year, int $lastYear, int $month, int $lastMonth, string $day = 'Monday'): int
     {
         if ($year > $lastYear  ($year == $lastYear && $month >= $lastMonth)  $year < 0) {
-        throw new \InvalidArgumentException();
+            throw new \InvalidArgumentException();
         }
         $result = 0;
         $start = strtotime($year . '-' . $month . '-' . '01');
