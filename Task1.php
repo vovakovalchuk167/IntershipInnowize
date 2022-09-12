f25fe7a2dff6c28b5f8831d2f2972eff8f536145
Oleg339 <44521022+Oleg339@users.noreply.github.com>
2022-09-12 11:55:22 +0300

    Update Task1.php

--- Task1.php
+++ Task1.php
@@ -12,6 +12,7 @@ class Task1
         $returnValue = ($number <= 10) ? 'Equal or less than 10' : '';
         $returnValue = ($number > 10) ? 'More than 10' : $returnValue;
         $returnValue = ($number > 20) ? 'More than 20' : $returnValue;
+
         return ($number > 30) ? 'More than 30' : $returnValue;
     }
 }
