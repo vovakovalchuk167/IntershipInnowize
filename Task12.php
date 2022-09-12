5ecc0697c143653815ed3fc92f6abd04353d4065
Oleg339 <44521022+Oleg339@users.noreply.github.com>
2022-09-12 22:22:32 +0300

    Add files via upload

---
+++ Task12.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace src;
+
+class Task12
+{
+    private int $a;
+    private int $b;
+
+    public function __construct(int $a, int $b)
+    {
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    public function add(): Task12
+    {
+        return new Task12($this->a + $this->b, 0);
+    }
+
+    public function multiply(): Task12
+    {
+        return new Task12($this->a * $this->b, 0);
+    }
+
+    public function divide(): Task12
+    {
+        if ($this->b === 0) {
+            throw new \InvalidArgumentException();
+        }
+
+        return new Task12($this->a / $this->b, 0);
+    }
+
+    public function subtract(): Task12
+    {
+        return new Task12($this->a - $this->b, 0);
+    }
+
+    public function addBy(int $num): Task12
+    {
+        return new Task12($this->a + $num, 0);
+    }
+
+    public function divideBy(int $num): Task12
+    {
+        if ($num === 0) {
+            throw new \InvalidArgumentException();
+        }
+
+        return new Task12($this->a / $num, 0);
+    }
+
+    public function multiplyBy(int $num): Task12
+    {
+        return new Task12($this->a * $num, 0);
+    }
+
+    public function subtractBy(int $num): Task12
+    {
+        return new Task12($this->a - $num, 0);
+    }
+
+    public function __toString(): string
+    {
+        return $this->a;
+    }
+}
