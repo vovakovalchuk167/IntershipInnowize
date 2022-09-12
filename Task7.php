aeaed459704e686bad7637d0ed2778d56da9c91d
Oleg339 <44521022+Oleg339@users.noreply.github.com>
2022-09-12 18:25:44 +0300

    Add files via upload

---
+++ Task7.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace src;
+
+class Task7
+{
+    public function main(array $arr, int $position): array
+    {
+        if ($position < 0 || $position > count($arr) - 1 || sizeof($arr) == 0) {
+            throw new \InvalidArgumentException();
+        }
+        for ($i = $position; $i < count($arr) - 1; $i++) {
+            $arr[$i] = $arr[$i + 1];
+        }
+
+        return array_slice($arr, 0, count($arr) - 1);
+    }
+}
